021486739686b978c44c9fbc54e69d863a82945c
19ATF72 <micah.hobby@gmail.com>
2021-03-19 16:08:01 +0000

    adding database & graphs

--- background_scripts/dynamic_dao.js
+++ background_scripts/dynamic_dao.js
@@ -1,2 +1,13 @@
-// DAO connection to database here
-console.log("DAO loaded");
+// // DAO connection to database here
+// console.log("DAO loaded");
+//
+// function logCookie(c) {
+//   console.log(c);
+// }
+//
+// function logError(e) {
+//   console.error(e);
+// }
+//
+// var setCookie = browser.cookies.set({ url: "https://developer.mozilla.org/" });
+// setCookie.then(logCookie, logError);
